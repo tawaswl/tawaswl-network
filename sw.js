5b1a8e3f49b86cf77b8f735aea31043729071e56
tawaswl <tawaswl@outlook.com>
2025-02-19 02:09:52 +0300

    تحديث Service Worker

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
 const CACHE_NAME = 'tawaswl-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json'
+  '.',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-72x72.png',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  './logo.png',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'
 ];
 
 self.addEventListener('install', event => {
